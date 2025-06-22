aa1d6d892dd4dc88beddfeab87dac32fd48227ca
karoldayansolerorozco <k.soler@uniandes.edu.co>
2025-06-21 23:30:01 -0500

    Segundo comentario

--- canny.py
+++ canny.py
@@ -4,6 +4,7 @@ import cv2
 img = cv2.imread('/home/dayan/Documentos/Canny/moneda.jpg', 0)
 bordeCanny = cv2.Canny(img, 100, 200)
 
+#Muestra la imagen
 cv2.imshow('Original', img)
 cv2.imshow('Canny', bordeCanny)
 
